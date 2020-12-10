d22e563ff66088946cfddc952d6bfb19c59805ad
santhibatchu <75074620+santhibatchu@users.noreply.github.com>
2020-12-10 18:33:55 +0530

    Create Currency.py
    give a command line argument

---
+++ Currency.py
@@ -0,0 +1,12 @@
+import sys
+
+r = int(sys.argv[1])
+
+div = (r // 17)
+b = 2 *div
+temp = r - ( div * 17)
+j = 2 * b
+left_amount = temp - (j // 2)
+p = left_amount * 26
+result = str(b)+str(j)+str(p)
+print(result)
